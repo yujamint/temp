7ca51f579a2ae396b419b144d27118074e8ee797
yujaemin <woawoa0513@gmail.com>
2022-08-14 15:28:35 +0900

    동시편집 기능 구현

--- websocket/app.js
+++ websocket/app.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -19,17 +20,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//방 변수들
+let pairmamber = 2;
+let roomId = 1;
+let order = 1;
+
 //새 웹 소켓 접속시
 app.io.on("connection", (socket) => {
     console.log("새로 접속.");
+    
+    //방만들기
+    console.log(roomId);
+    socket.join(roomId);
+    socket.emit("roomIdPass",roomId);
+    // roomID -> socket.id로해도될듯
+
+    if(order % pairmamber == 0){
+        roomId++;
+    }
+    order++;
+    
+
     socket.on("disconnect", () => {
         console.log("접속 끊어짐.");
     });
 
+    
     socket.on("update", (data) => {
-        console.log(data.event, data.delta);
-
-        socket.broadcast.emit("update", data);
+        console.log(data.event, data.delta, data.roomId);
+        
+        socket.to(data.roomId).emit("update", data);
     })
 })
 
